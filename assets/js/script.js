8ed3033fd569067b1d757a9e7ea96ab324d90b33
Dominika <domi_nika@hotmail.co.uk>
2021-04-29 16:02:44 +0100

    modal renders but with incorrect info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,51 @@
-const devProjects = 
-`<div class="col-md-6">
-  <img class="portfolio-image" src="./assets/images/my-photo.jpeg" alt="image of Dominika Pietrzak">
-  <h3> NAME OF PROJECT </h3>
-  <button class="portfolio__button" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> Find out more </button>
-</div>
-<div class="col-md-6">
-  <img class="portfolio-image" src="./assets/images/my-photo.jpeg" alt="image of Dominika Pietrzak">
-  <h3> NAME OF PROJECT </h3>
-  <button class="portfolio__button" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> Find out more </button>
-</div>
-</section>`
+const devProjectsArray = [
+  {
+  name: "Event Jam",
+  image: "./assets/images/my-photo.jpeg",
+  description: "Event Jam was a group project",
+  repoUrl: `https://github.com/iwanagahime/event-jam`,
+  deployedUrl: `https://iwanagahime.github.io/event-jam/`,
+  },
+  {
+    name: "Project 2",
+    image: "./assets/images/my-photo.jpeg",
+    description: "Event Jam was a group project",
+    repoUrl: `https://github.com/iwanagahime/event-jam`,
+    deployedUrl: `https://iwanagahime.github.io/event-jam/`,
+    }
+  ];
+
+const buildDevProjectCard = (item) => {
+  $("#selected-work-container").append(
+    `<div class="col-md-6">
+      <img class="portfolio-image" src=${item.image} alt="image of ${item.name}">
+      <h3>${item.name}</h3>
+      <button class="portfolio__button" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"
+      data-name="${item.name}" data-image="${item.name}" data-description="${item.description}" data-repoUrl="${item.repoUrl}" data-deployedUrl="${item.deployedUrl}"> 
+        Find out more 
+      </button>
+    </div>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+      <div class="offcanvas-header">
+      <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+      <h1>${item.name}</h1>
+      <h2>In a nutshell</h2>
+      <p>${item.description}</p>
+      <button> View Deployed App </button>
+      <button> View on Github </button>
+      </div>
+    </div>`
+  )
+}
 
 const renderDevProjects = () => {
   $("#selected-work-container").empty()
-  $("#selected-work-container").append(devProjects)
+  devProjectsArray.forEach(buildDevProjectCard)
+  // $("#selected-work-container").append(devProjects)
+  console.log(devProjectsArray[0].name)
 }
 
 const renderDesignProjects = () => {
