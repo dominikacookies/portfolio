bab4ec54fdf3d004cd658b5ed610e1d72167ee82
Dominika <domi_nika@hotmail.co.uk>
2021-04-29 14:44:09 +0100

    added event listeners for dev and design buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const renderDevProjects = () => {
+  console.log("hello")
+}
+
+const renderDesignProjects = () => {
+  console.log("hey")
+}
+
 $("#cv-button").click(function() {
   window.open("./assets/cv/DP_CV.pdf", "_blank")
 });
@@ -5,3 +13,6 @@ $("#cv-button").click(function() {
 $("#github-button").click(function() {
   window.open("https://github.com/dominikacookies", "_blank")
 });
+
+$("#dev-projects-button").click(renderDevProjects)
+$("#design-projects-button").click(renderDesignProjects)
