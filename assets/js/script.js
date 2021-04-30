97db875c37bbdaa92b7d724541f684bc3dba803c
Dominika <domi_nika@hotmail.co.uk>
2021-04-30 22:38:49 +0100

    renders design projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const devProjectsArray = [
   {
   name: "Event Jam",
-  image: "./assets/images/eventJamScreen.png",
+  image: "./assets/images/devprojects/eventJamScreen.png",
   description: "Event Jam is an online application which allows users to search for events and COIVD-19 information in any city in the UK by retrieving information from the Government's COVID-19 Dashboard and Ticketmaster APIs. The application visually displays the number of COVID-19 cases identfied in a given area using the chart.js framework, allows users to book tickets for an event by redirecting them to the relevant Ticketmaster page as well as to save the event to their favourites using the persistent memory of local storage.   ",
   repoUrl: `https://github.com/iwanagahime/event-jam`,
   deployedUrl: `https://iwanagahime.github.io/event-jam/`,
@@ -9,13 +9,26 @@ const devProjectsArray = [
   },
   {
     name: "Weather Wizard",
-    image: "./assets/images/weatherApp.png",
+    image: "./assets/images/devprojects/weatherApp.png",
     description: "Weather Wizard is an online application which allows the user to check the current and forecasted weather in a city of their choice. It saves the user's most recent searches using the persistent memory of local storage to create a sleek and effortless user experience. The application retrieves weather information using the Open Weather API and colour codes the UV index to inform the user whether the conditions are favourable, moderate or severe.",
     repoUrl: `https://github.com/dominikacookies/weather_forecast_app`,
     deployedUrl: `https://dominikacookies.github.io/weather_forecast_app/`,
     modalName: "weather-wizard",
     },
   ];
+
+const designProjectsArray = [
+  {
+    name: "Ecommerce Home Page Design",
+    client: "fictional stationary retailer",
+    image: "./assets/images/designprojects/stil-design.jpg",
+  },
+  {
+    name: "Agency Home Page Design",
+    client: "fictional web design agency",
+    image: "./assets/images/designprojects/HEI.jpg",
+  },
+]
 
 const goToApp = (event) => {
   const deployedAppUrl = $(event.currentTarget).attr("data-deployedUrl")
@@ -53,15 +66,27 @@ const buildDevProjectCard = (item) => {
   )
 }
 
-const renderDevProjects = () => {
+const displayDesignProject = (item) => {
+  $("#selected-work-container").append(
+    `<div class="col-md-6">
+      <img class="portfolio-image" src=${item.image} alt="image of ${item.name}">
+      <h3>${item.name}</h3>
+      <h4> Designed for a ${item.client}</h4>
+    </div>`)
+  }
+
+const renderDevProjects = (event) => {
+  $(event.currentTarget).addClass("button--secondary")
   $("#selected-work-container").empty()
   devProjectsArray.forEach(buildDevProjectCard)
   $(".deployed-button").click(goToApp)
   $(".repo-button").click(goToRepo)
 }
 
-const renderDesignProjects = () => {
-  console.log("hey")
+const renderDesignProjects = (event) => {
+  $("#selected-work-container").empty()
+  $(event.currentTarget).addClass("button--secondary")
+  designProjectsArray.forEach(displayDesignProject)
 }
 
 $("#cv-button").click(function() {
