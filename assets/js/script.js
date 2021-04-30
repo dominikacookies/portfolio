0530c723ab22c80c3dc3e7d4aca5509e23fedee1
Dominika <domi_nika@hotmail.co.uk>
2021-04-30 12:22:32 +0100

    styled slider buttons and added project descriptors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const devProjectsArray = [
   {
   name: "Event Jam",
   image: "./assets/images/eventJamScreen.png",
-  description: "Event Jam was a group project",
+  description: "Event Jam is an online application which allows users to search for events and COIVD-19 information in any city in the UK by retrieving information from the Government's COVID-19 Dashboard and Ticketmaster APIs. The application visually displays the number of COVID-19 cases identfied in a given area using the chart.js framework, allows users to book tickets for an event by redirecting them to the relevant Ticketmaster page as well as to save the event to their favourites using the persistent memory of local storage.   ",
   repoUrl: `https://github.com/iwanagahime/event-jam`,
   deployedUrl: `https://iwanagahime.github.io/event-jam/`,
   modalName: "event-jam",
@@ -10,7 +10,7 @@ const devProjectsArray = [
   {
     name: "Weather Wizard",
     image: "./assets/images/weatherApp.png",
-    description: "Event Jam was a group project",
+    description: "Weather Wizard is an online application which allows the user to check the current and forecasted weather in a city of their choice. It saves the user's most recent searches using the persistent memory of local storage to create a sleek and effortless user experience. The application retrieves weather information using the Open Weather API and colour codes the UV index to inform the user whether the conditions are favourable, moderate or severe.",
     repoUrl: `https://github.com/iwanagahime/event-jam`,
     deployedUrl: `https://dominikacookies.github.io/weather_forecast_app/`,
     modalName: "weather-wizard",
@@ -37,15 +37,15 @@ const buildDevProjectCard = (item) => {
     </div>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="${item.modalName}" aria-labelledby="offcanvasRightLabel">
       <div class="offcanvas-header">
-      <h5 id="offcanvasRightLabel">Offcanvas right</h5>
       <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
       <h1>${item.name}</h1>
       <h2>In a nutshell</h2>
       <p class="information">${item.description}</p>
-      <button class="portfolio__button deployed-button"> Deployed App </button>
-      <button class="portfolio__button repo-button"> Github Repo </button>
+      <h2 class="mt-2">Check it out</h2>
+      <button class="portfolio--application__button deployed-button"> Deployed App </button>
+      <button class="portfolio--application__button repo-button"> Github Repo </button>
       </div>
     </div>`
   )
