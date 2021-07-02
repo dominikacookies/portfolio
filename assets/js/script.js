68209936e565671fceadede45ba09e683d20ea4a
Dominika <domi_nika@hotmail.co.uk>
2021-07-02 17:20:34 +0100

    replaced col based cards with ones that work inside a flex box and added new projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,36 @@
 const devProjectsArray = [
   {
   name: "Event Jam",
-  image: "./assets/images/devprojects/eventJamScreen.png",
+  image: "./assets/images/devprojects/event-jam.png",
   description: "Event Jam is an online application which allows users to search for events and COIVD-19 information in any city in the UK by retrieving information from the Government's COVID-19 Dashboard and Ticketmaster APIs. The application visually displays the number of COVID-19 cases identfied in a given area using the chart.js framework, allows users to book tickets for an event by redirecting them to the relevant Ticketmaster page as well as to save the event to their favourites using the persistent memory of local storage.   ",
   repoUrl: `https://github.com/iwanagahime/event-jam`,
   deployedUrl: `https://iwanagahime.github.io/event-jam/`,
   modalName: "event-jam",
   },
   {
+    name: "Vegan Pals",
+    image: "./assets/images/devprojects/vegan-pals.png",
+    description: "Vegan pals is an online website, designed to help individuals with food intolerances discover vegan dishes that suit their dietry requirements. Additionally, the application allows users to save their favourite recipes and arrange to cook them with other users that hold the same intolerances by connecting them together.",
+    repoUrl: `https://github.com/dominikacookies/vegan-pals-2.0`,
+    deployedUrl: `https://vegan-pals-2.herokuapp.com/`,
+    modalName: "vegan-pals",
+  },
+  {
     name: "Weather Wizard",
-    image: "./assets/images/devprojects/weatherApp.png",
+    image: "./assets/images/devprojects/weather-app.png",
     description: "Weather Wizard is an online application which allows the user to check the current and forecasted weather in a city of their choice. It saves the user's most recent searches using the persistent memory of local storage to create a sleek and effortless user experience. The application retrieves weather information using the Open Weather API and colour codes the UV index to inform the user whether the conditions are favourable, moderate or severe.",
     repoUrl: `https://github.com/dominikacookies/weather_forecast_app`,
     deployedUrl: `https://dominikacookies.github.io/weather_forecast_app/`,
     modalName: "weather-wizard",
     },
-    {
-      name: "Javascript Quiz",
-      image: "./assets/images/devprojects/javascriptQuiz.png",
-      description: "The Javascript Quiz is an online quiz that allows users to test their basic javascript knowledge. Users race against the clock to answer 5 Javascript related questions, every correct choice highlights the chosen answer in green and adds points to the user's score. An incorrect answer will inturn be highlighted in red and deduct time from the countdown clock. Users are als able to save and later view their highscores.",
-      repoUrl: `https://github.com/dominikacookies/js-quiz`,
-      deployedUrl: `https://dominikacookies.github.io/js-quiz/`,
-      modalName: "jz-quiz",
-      },
+  {
+    name: "Tech Blog",
+    image: "./assets/images/devprojects/tech-blog.png",
+    description: "Vegan pals is an online website, designed to help individuals with food intolerances discover vegan dishes that suit their dietry requirements. Additionally, the application allows users to save their favourite recipes and arrange to cook them with other users that hold the same intolerances by connecting them together.",
+    repoUrl: `https://github.com/dominikacookies/tech-blog`,
+    deployedUrl: `https://fathomless-plains-35453.herokuapp.com/`,
+    modalName: "tech-blog",
+  },
   ];
 
 const designProjectsArray = [
@@ -50,17 +58,17 @@ const goToRepo = (event) => {
 
 const buildDevProjectCard = (item) => {
   $("#selected-work-container").append(
-    `<div class="col-md-4">
-      <img class="portfolio-image" src=${item.image} alt="image of ${item.name}">
-      <h3>${item.name}</h3>
-      <button class="portfolio__button" type="button" data-bs-toggle="offcanvas" data-bs-target="#${item.modalName}" aria-controls="offcanvasRight${item.name}"
-      data-name="${item.name}" data-image="${item.name}" data-description="${item.description}" data-repoUrl="${item.repoUrl}" data-deployedUrl="${item.deployedUrl}"> 
-        Find out more 
-      </button>
+    `
+    <div class="card project-type-card">
+      <img src=${item.image} class="card-img-top">
+      <div class="card-body">
+        <h2 class="card-title pb-3">${item.name}</h2>
+        <button class="button--primary" data-bs-toggle="offcanvas" data-bs-target="#${item.modalName}" aria-controls="offcanvasRight${item.name}">View now </button>
+      </div>
     </div>
     <div class="offcanvas offcanvas-end offcanvas-background-override" tabindex="-1" id="${item.modalName}" aria-labelledby="offcanvasRightLabel">
       <div class="offcanvas-header">
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="btn" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
       <h1>${item.name}</h1>
@@ -102,12 +110,12 @@ $("#linkedin-button").click(function() {
 });
 
 $("#cv-button").click(function() {
-  window.open("./assets/cv/CV_DP.pdf", "_blank")
+  window.open("./assets/cv/CV_CG_DP.pdf", "_blank")
 });
 
 $("#github-button").click(function() {
   window.open("https://github.com/dominikacookies", "_blank")
 });
 
-$("#dev-projects-button").click(renderDevProjects)
-$("#design-projects-button").click(renderDesignProjects)
+$("#dev-projects-card").click(renderDevProjects)
+$("#design-projects-card").click(renderDesignProjects)
